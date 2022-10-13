553e32226023b3232014fe56d274daa9e0327142
Christian <christian.galvez.hurtado@hotmail.com>
2022-10-13 13:12:19 +0200

    Otra vez

--- S1T4N1Ex2/CalculDni.java
+++ S1T4N1Ex2/CalculDni.java
@@ -2,4 +2,11 @@ package S1T4N1Ex2;
 
 public class CalculDni {
 
+	public static String calcularLetra(int numSinLetra) {
+		
+		if(numSinLetra
+		
+		
+		return null;
+	}
 }
--- S1T4N1Ex2/S1T4N1Ex2Main.java
+++ S1T4N1Ex2/S1T4N1Ex2Main.java
@@ -1,10 +1,47 @@
 package S1T4N1Ex2;
 
+import java.util.Scanner;
+
 public class S1T4N1Ex2Main {
 
+	static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		/*- Exercici 2
+		Crea una classe anomenada CalculoDni que calculi la lletra del DNI en rebre el número com a paràmetre.
+		Crea una classe jUnit que verifiqui el seu correcte funcionament,
+		parametritzant-la perquè el test rebi un espectre de dades ampli i validi si el càlcul és correcte per a 10 números de DNI predefinits.*/
+		
+		String msje = dni();
+		
+		
+		
+	}
+	
+	public static boolean comprobarNumeroValido(Integer iNumero) {
+		boolean cantidadCorrecta;
+		String x = Integer.toString(iNumero);
+		if
+		
+		return cantidadCorrecta;
 	}
 
+	public static String dni() {
+		int numDni;
+		boolean numVale, dniCorrecto;
+		
+		do {
+			System.out.println("Introduce el numero de DNI:");
+			numDni = scan.nextInt();
+			numVale = comprobarNumeroValido(numDni);
+			if(numVale = true) {
+			
+			}else {
+			
+			}
+		}while(dniCorrecto = false);
+		
+		return "";
+	}
 }
